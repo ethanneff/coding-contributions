22d26fc79e12b7d60e5436e3d591db4c94424c72
Ethan Neff <ethan.neff@eneff.com>
2021-07-29 12:59:37 -0700

    fix: update examples with correct prod baseurl

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,12 +1,12 @@
-export default (_, res) => {
+const API = (_, res) => {
   const env = process.env.NODE_ENV;
   const dev = env === "development";
   const baseUrl = dev
     ? "http://localhost:3333/"
-    : "https://github-contributions-json-api.now.sh/";
+    : "https://coding-contributions.vercel.app/api/";
   const username = "ethanneff";
   res.status(400).json({
-    error: "invalid api access",
+    message: "sample api access",
     examples: [
       `${baseUrl}api/${username}/github`,
       `${baseUrl}api/${username}/leetcode`,
@@ -14,3 +14,5 @@ export default (_, res) => {
     ],
   });
 };
+
+export default API;
