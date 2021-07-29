418ecfe96219a7880e63eb8c2e42d9a8bb316850
Ethan Neff <ethan.neff@eneff.com>
2021-07-29 12:59:25 -0700

    feat: add home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Home = () => (
+  <div>
+    <h1>Coding Contributions</h1>
+    <Link href="/api">
+      <a>View API</a>
+    </Link>
+  </div>
+);
+
+export default Home;
